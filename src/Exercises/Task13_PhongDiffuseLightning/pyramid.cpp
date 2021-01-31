41c4a317ffd58aff25996c71c2b7405f2058beb9
Łukasz Rydziński <rlukasz1996@gmail.com>
2021-01-31 23:30:14 +0100

    || Task 13 Part 2 (Normals added)

--- src/Exercises/Task13_PhongDiffuseLightning/pyramid.cpp
+++ src/Exercises/Task13_PhongDiffuseLightning/pyramid.cpp
@@ -14,17 +14,11 @@ Pyramid::Pyramid() {
 
     std::vector<GLfloat> vertices = {
             // positions      // texture coords // normals
-            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,     //0.0f, 0.0f, 1.0f,
-            1.0f, -1.0f, 0.0f, 1.0f, 0.0f,      //0.0f, 0.0f, 1.0f,
-            1.0f, 1.0f, 0.0f, 1.0f, 1.0f,      // 0.0f, 0.0f, 1.0f,
-            -1.0f, 1.0f, 0.0f, 0.0f, 1.0f//,      //0.0f, 0.0f, 1.0f,
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,     0.0f, 0.0f, 1.0f,
+            1.0f, -1.0f, 0.0f, 1.0f, 0.0f,      0.0f, 0.0f, 1.0f,
+            1.0f, 1.0f, 0.0f, 1.0f, 1.0f,       0.0f, 0.0f, 1.0f,
+            -1.0f, 1.0f, 0.0f, 0.0f, 1.0f,      0.0f, 0.0f, 1.0f
     };
-
-    GLuint v_buffer_handle;
-    glGenBuffers(1, &v_buffer_handle);
-    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     //*********************
 
@@ -33,34 +27,29 @@ Pyramid::Pyramid() {
             0,1,2,2,3,0// wypisujemy tyle elementów ile mamy wierzchołków
     };
 
-    GLuint idx_buffer_handle;
-    glGenBuffers(1,&idx_buffer_handle);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(),
-                 GL_STATIC_DRAW);
+    glGenBuffers(2, buffer_);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer_[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, buffer_[1]);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glGenVertexArrays(1, &vao_);
     glBindVertexArray(vao_);
-    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer_[1]);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer_[0]);
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(5 * sizeof(GLfloat)));
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-
-    glGenBuffers(2,buffer_);
-
-    glBindBuffer(GL_UNIFORM_BUFFER, buffer_[0]);
-    glBufferData(GL_UNIFORM_BUFFER, 8 * sizeof(float), nullptr, GL_STATIC_DRAW);
-    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(float),&vertices);
-    glBufferSubData(GL_UNIFORM_BUFFER, 4 * sizeof(float), 3 * sizeof(float),&indices);
-    glBindBuffer(GL_UNIFORM_BUFFER, 0);
-    glBindBufferBase(GL_UNIFORM_BUFFER, 0, buffer_[0]);
-    glBindBuffer(GL_UNIFORM_BUFFER, buffer_[1]);
 
     //*********************
 
@@ -96,11 +85,11 @@ Pyramid::~Pyramid() {
     //Tu usuwamy VAO i bufory
 
     if(vao_) {
-        delete[] (char*)vao_;
+        glDeleteVertexArrays(1,&vao_);
     }
 
     if(buffer_)
-        delete[] (char*)buffer_;
+        glDeleteBuffers(2, buffer_);
 
 }
 
--- src/Exercises/Task13_PhongDiffuseLightning/shaders/base_vs.glsl
+++ src/Exercises/Task13_PhongDiffuseLightning/shaders/base_vs.glsl
@@ -6,6 +6,7 @@ out vec2 TexCoord;
 layout(location=0) in  vec4 a_vertex_position;
 //layout(location=1) in vec3 a_color;
 layout (location = 1) in vec2 aTexCoord;
+layout(location=2) in  vec3 a_vertex_normal;
 
 layout(std140) uniform Matrixes {
     mat4 PVM;
